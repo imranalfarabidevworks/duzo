35fa22fda51264b77233e5f01ade1fe3e3719ed5
Imran Al Farabi <imranalfarabidevworks@gmail.com>
2026-07-02 13:31:31 +0600

    added some

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'i.ibb.co' }, // ImgBB এর ডাইরেক্ট ইমেজ ডোমেইন
     ],
   },
 };
